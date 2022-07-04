53d3543d85b166e96a28954fa679ce603738ae93
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-07-04 19:25:59 +0530

    Add files via upload

---
+++ NobodyCanCreateObject.java
@@ -0,0 +1,30 @@
+/*
+ * @Author:OWASHIM CHOWDHURY
+ * 
+ * java program of no one can create object from outside the class
+ * 
+ * here i have used private in constructor thats why object creation from outside the class is not possible 
+ */
+
+package Assignment;
+
+class Object{
+	int id;
+	String name;
+	private Object(int id,String name){  	//private constructor
+		this.id=id;
+		this.name=name;
+	}
+	void display() {
+		System.out.println(id+ " "+name);		
+	}	
+}
+ public class NobodyCanCreateObject{
+ public static void main(String[]args) {
+ Object sc =new Object(2,"owashim");	//
+ sc.display();
+ 
+ }	//end of main method
+ }	//end of class
+ 
+ 
