a1a2738cff51c1fc9b69b4057e905dc0421498a6
Owashim Chowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-09-14 09:31:07 +0530

    Create ThreadListLoaderLab.java

---
+++ ThreadsActivity/ThreadListLoaderLab.java
@@ -0,0 +1,34 @@
+package Lab;
+/*
+ * @ owashim chowdhury
+ */
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+
+
+public class ThreadListLoaderLab implements Runnable{
+	public void run() {
+		
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+		//for loop
+		for (int i = 0; i <= 1000000; i++)
+		{
+			//adding values
+		   numbers.add(i);
+		   System.out.println(numbers.get(i));
+		}	
+		//calculating time
+		System.out.println("time taken in ms:"+System.currentTimeMillis ());
+	}
+
+		public static void main(String[] args) {
+			Runnable r= new ThreadListLoaderLab();
+		
+			Thread t=new Thread(r);//creating object
+			t.start();
+			
+		}
+	
+	}
